a53cc397ec099ec3f977d81b70677691d25c4a4d
JakubNiszczyciel <jakub.niszczyciel.swiatow@gmail.com>
2017-04-08 23:48:54 +0200

    Version 0.1
    Added 2 InAnimations (Rotate, Bounce),
    Added 1 OutAnimation (Rotate)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*-------------------
-     MOJE SKRYPTY  
+     Just some scripts that you just don't need. :P  
 --------------------*/
 var timeoutHandle = window.setTimeout(function(){document.getElementById("animation-show").setAttribute("class", "");}, 800);
 
@@ -17,9 +17,30 @@ document.getElementById("FadeInLeft").addEventListener("click", function () {cha
 document.getElementById("FadeInRight").addEventListener("click", function () {change_active_aniamtion("FadeInRight");});
 document.getElementById("FadeInTop").addEventListener("click", function () {change_active_aniamtion("FadeInTop");});
 document.getElementById("FadeInBottom").addEventListener("click", function () {change_active_aniamtion("FadeInBottom");});
+document.getElementById("FadeInBounceLeft").addEventListener("click", function () {change_active_aniamtion("FadeInBounceLeft");});
+document.getElementById("FadeInBounceRight").addEventListener("click", function () {change_active_aniamtion("FadeInBounceRight");});
+document.getElementById("FadeInBounceTop").addEventListener("click", function () {change_active_aniamtion("FadeInBounceTop");});
+document.getElementById("FadeInBounceBottom").addEventListener("click", function () {change_active_aniamtion("FadeInBounceBottom");});
+document.getElementById("FadeInRotateLeftTop").addEventListener("click", function () {change_active_aniamtion("FadeInRotateLeftTop");});
+document.getElementById("FadeInRotateLeftBottom").addEventListener("click", function () {change_active_aniamtion("FadeInRotateLeftBottom");});
+document.getElementById("FadeInRotateRightTop").addEventListener("click", function () {change_active_aniamtion("FadeInRotateRightTop");});
+document.getElementById("FadeInRotateRightBottom").addEventListener("click", function () {change_active_aniamtion("FadeInRotateRightBottom");});
 
 // OutAnimations
 document.getElementById("FadeOutLeft").addEventListener("click", function () {change_active_aniamtion("FadeOutLeft");});
 document.getElementById("FadeOutRight").addEventListener("click", function () {change_active_aniamtion("FadeOutRight");});
 document.getElementById("FadeOutTop").addEventListener("click", function () {change_active_aniamtion("FadeOutTop");});
 document.getElementById("FadeOutBottom").addEventListener("click", function () {change_active_aniamtion("FadeOutBottom");});
+document.getElementById("FadeOutRotateLeftTop").addEventListener("click", function () {change_active_aniamtion("FadeOutRotateLeftTop");});
+document.getElementById("FadeOutRotateLeftBottom").addEventListener("click", function () {change_active_aniamtion("FadeOutRotateLeftBottom");});
+document.getElementById("FadeOutRotateRightTop").addEventListener("click", function () {change_active_aniamtion("FadeOutRotateRightTop");});
+document.getElementById("FadeOutRotateRightBottom").addEventListener("click", function () {change_active_aniamtion("FadeOutRotateRightBottom");});
+
+
+
+
+
+
+
+
+
