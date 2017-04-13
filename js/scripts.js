0801a74353b6699c5d47dbd8faae8e94aef28b5c
JakubNiszczyciel <jakub.niszczyciel.swiatow@gmail.com>
2017-04-13 18:25:26 +0200

    Version 0.2.1
    Opimalized JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,83 +2,58 @@
      Just some scripts that you just don't need. :P  
 --------------------*/
 // Menu
-var InAnimations = document.getElementById("InAnimationsMenu");
-var OutAnimations = document.getElementById("OutAnimationsMenu");
+var InAnimationsMenu = document.getElementById("InAnimationsMenu"),
+	OutAnimationsMenu = document.getElementById("OutAnimationsMenu"),
+	InAnimations = document.getElementById("InAnimations"),
+	OutAnimations = document.getElementById("OutAnimations"),
+	Animation_Show = document.getElementById("animation-show"),
+	Animation_Wrapper = document.getElementById("animation-wrapper"),
+	timeoutHandle = window.setTimeout(function(){});
 
-InAnimations.addEventListener("click", function(){
-	InAnimations.setAttribute("class","ActiveMenu");
-	OutAnimations.setAttribute("class",""); 
-	document.getElementById("InAnimations").setAttribute("class","ActiveAnimations");
-	document.getElementById("OutAnimations").setAttribute("class","");
-})
-OutAnimations.addEventListener("click", function(){
-	InAnimations.setAttribute("class","");
-	OutAnimations.setAttribute("class","ActiveMenu"); 
-	document.getElementById("InAnimations").setAttribute("class","");
-	document.getElementById("OutAnimations").setAttribute("class","ActiveAnimations");
-})
-
-var timeoutHandle = window.setTimeout(function(){document.getElementById("animation-show").setAttribute("class", "");}, 800);
-
-function change_active_aniamtion(id_of_animation) {
-	document.getElementById("animation-show").setAttribute("class", "");
-	window.clearTimeout(timeoutHandle);
-	var animation_name = document.getElementById(id_of_animation).textContent;
-	document.getElementsByClassName("active")[0].setAttribute("class", "");
-	document.getElementById(id_of_animation).setAttribute("class", "active");
-	
-	var vertical_adjustmentLeft = animation_name.includes("Left");
-	var vertical_adjustmentRight = animation_name.includes("Right");
-	var horizontal_adjustmentTop = animation_name.includes("Top");
-	var horizontal_adjustmentBottom = animation_name.includes("Bottom");
-	
-	if(vertical_adjustmentLeft == true)	document.getElementById("animation-wrapper").setAttribute("class", "perspective-origin-left")
-	else if(vertical_adjustmentRight == true) document.getElementById("animation-wrapper").setAttribute("class", "perspective-origin-right")
-	else if(horizontal_adjustmentTop == true) document.getElementById("animation-wrapper").setAttribute("class", "perspective-origin-top")
-	else if(horizontal_adjustmentBottom == true) document.getElementById("animation-wrapper").setAttribute("class", "perspective-origin-bottom")
-	else document.getElementById("animation-wrapper").setAttribute("class", "")
-	
-	timeoutHandle = window.setTimeout(function(){document.getElementById("animation-show").setAttribute("class", id_of_animation);}, 1);;
+function ActiveMenu() {
+	document.getElementsByClassName("ActiveMenu")[0].className =  "";
+	this.className = "ActiveMenu";
+	if (this.textContent.includes("In")) {
+		InAnimations.className = "ActiveAnimations";
+		OutAnimations.className = "";
+	} else {
+		InAnimations.className = "";
+		OutAnimations.className = "ActiveAnimations";
+	}
 }
-// InAnimations
-document.getElementById("InFadeLeft").addEventListener("click", function () {change_active_aniamtion("InFadeLeft");});
-document.getElementById("InFadeRight").addEventListener("click", function () {change_active_aniamtion("InFadeRight");});
-document.getElementById("InFadeTop").addEventListener("click", function () {change_active_aniamtion("InFadeTop");});
-document.getElementById("InFadeBottom").addEventListener("click", function () {change_active_aniamtion("InFadeBottom");});
-document.getElementById("InFadeBounceLeft").addEventListener("click", function () {change_active_aniamtion("InFadeBounceLeft");});
-document.getElementById("InFadeBounceRight").addEventListener("click", function () {change_active_aniamtion("InFadeBounceRight");});
-document.getElementById("InFadeBounceTop").addEventListener("click", function () {change_active_aniamtion("InFadeBounceTop");});
-document.getElementById("InFadeBounceBottom").addEventListener("click", function () {change_active_aniamtion("InFadeBounceBottom");});
-document.getElementById("InFadeRotateLeftTop").addEventListener("click", function () {change_active_aniamtion("InFadeRotateLeftTop");});
-document.getElementById("InFadeRotateLeftBottom").addEventListener("click", function () {change_active_aniamtion("InFadeRotateLeftBottom");});
-document.getElementById("InFadeRotateRightTop").addEventListener("click", function () {change_active_aniamtion("InFadeRotateRightTop");});
-document.getElementById("InFadeRotateRightBottom").addEventListener("click", function () {change_active_aniamtion("InFadeRotateRightBottom");});
-document.getElementById("InRotate3dLeftBack").addEventListener("click", function () {change_active_aniamtion("InRotate3dLeftBack");});
-document.getElementById("InRotate3dRightBack").addEventListener("click", function () {change_active_aniamtion("InRotate3dRightBack");});
-document.getElementById("InRotate3dTopBack").addEventListener("click", function () {change_active_aniamtion("InRotate3dTopBack");});
-document.getElementById("InRotate3dBottomBack").addEventListener("click", function () {change_active_aniamtion("InRotate3dBottomBack");});
-document.getElementById("InRotate3dLeftFront").addEventListener("click", function () {change_active_aniamtion("InRotate3dLeftFront");});
-document.getElementById("InRotate3dRightFront").addEventListener("click", function () {change_active_aniamtion("InRotate3dRightFront");});
-document.getElementById("InRotate3dTopFront").addEventListener("click", function () {change_active_aniamtion("InRotate3dTopFront");});
-document.getElementById("InRotate3dBottomFront").addEventListener("click", function () {change_active_aniamtion("InRotate3dBottomFront");});
+function finding_clicked_element(e) {
+    if (e.target !== e.currentTarget) {
+        var clickedItem = e.target;
+		change_active_aniamtion.call(clickedItem);
+    }
+    e.stopPropagation();
+}
+function change_active_aniamtion() {	
+	var animation_name = this.textContent,
+		vertical_adjustmentLeft = animation_name.includes("Left"),
+		vertical_adjustmentRight = animation_name.includes("Right"),
+		horizontal_adjustmentTop = animation_name.includes("Top"),
+		horizontal_adjustmentBottom = animation_name.includes("Bottom");
+		
+	Animation_Show.className = "";
+	window.clearTimeout(timeoutHandle);
+	document.getElementsByClassName("active")[0].className =  "";
+	this.className = "active";
 
-// OutAnimations
-document.getElementById("OutFadeLeft").addEventListener("click", function () {change_active_aniamtion("OutFadeLeft");});
-document.getElementById("OutFadeRight").addEventListener("click", function () {change_active_aniamtion("OutFadeRight");});
-document.getElementById("OutFadeTop").addEventListener("click", function () {change_active_aniamtion("OutFadeTop");});
-document.getElementById("OutFadeBottom").addEventListener("click", function () {change_active_aniamtion("OutFadeBottom");});
-document.getElementById("OutFadeRotateLeftTop").addEventListener("click", function () {change_active_aniamtion("OutFadeRotateLeftTop");});
-document.getElementById("OutFadeRotateLeftBottom").addEventListener("click", function () {change_active_aniamtion("OutFadeRotateLeftBottom");});
-document.getElementById("OutFadeRotateRightTop").addEventListener("click", function () {change_active_aniamtion("OutFadeRotateRightTop");});
-document.getElementById("OutFadeRotateRightBottom").addEventListener("click", function () {change_active_aniamtion("OutFadeRotateRightBottom");});
-document.getElementById("OutRotate3dLeftBack").addEventListener("click", function () {change_active_aniamtion("OutRotate3dLeftBack");});
-document.getElementById("OutRotate3dRightBack").addEventListener("click", function () {change_active_aniamtion("OutRotate3dRightBack");});
-document.getElementById("OutRotate3dTopBack").addEventListener("click", function () {change_active_aniamtion("OutRotate3dTopBack");});
-document.getElementById("OutRotate3dBottomBack").addEventListener("click", function () {change_active_aniamtion("OutRotate3dBottomBack");});
-document.getElementById("OutRotate3dLeftFront").addEventListener("click", function () {change_active_aniamtion("OutRotate3dLeftFront");});
-document.getElementById("OutRotate3dRightFront").addEventListener("click", function () {change_active_aniamtion("OutRotate3dRightFront");});
-document.getElementById("OutRotate3dTopFront").addEventListener("click", function () {change_active_aniamtion("OutRotate3dTopFront");});
-document.getElementById("OutRotate3dBottomFront").addEventListener("click", function () {change_active_aniamtion("OutRotate3dBottomFront");});
+	if(vertical_adjustmentLeft == true)	Animation_Wrapper.className = "perspective-origin-left";
+	else if(vertical_adjustmentRight == true) Animation_Wrapper.className = "perspective-origin-right";
+	else if(horizontal_adjustmentTop == true) Animation_Wrapper.className = "perspective-origin-top";
+	else if(horizontal_adjustmentBottom == true) Animation_Wrapper.className = "perspective-origin-bottom";
+	else Animation_Wrapper.className =  "";
+
+	timeoutHandle = window.setTimeout(function(){Animation_Show.className = animation_name}, 1);;	
+}
+
+InAnimationsMenu.addEventListener("click",ActiveMenu);
+OutAnimationsMenu.addEventListener("click",ActiveMenu);
+
+InAnimations.addEventListener("click", finding_clicked_element);
+OutAnimations.addEventListener("click", finding_clicked_element);
 
 
 
